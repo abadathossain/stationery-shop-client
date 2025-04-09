82e251dfe51e1c44117647a89a6a2f2cae8bf35c
Abadat Hossain <abadat26@gmail.com>
2025-04-09 10:04:58 +0600

    navbar setup done

--- src/components/home/Navbar.tsx
+++ src/components/home/Navbar.tsx
@@ -1,5 +1,30 @@
+import { Menubar, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <Menubar className="bg-blue-300 justify-end gap-10">
+        <MenubarMenu>
+          <Link to="/">
+            <MenubarTrigger>Home</MenubarTrigger>
+          </Link>
+          <Link to="/about">
+            <MenubarTrigger>About</MenubarTrigger>
+          </Link>
+          <Link to="/featured">
+            <MenubarTrigger>Featured</MenubarTrigger>
+          </Link>
+          <Link to="/products">
+            <MenubarTrigger>Products</MenubarTrigger>
+          </Link>
+          <Link to="/contact">
+            <MenubarTrigger>Contact</MenubarTrigger>
+          </Link>
+        </MenubarMenu>
+      </Menubar>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,6 +1,10 @@
+import Home from "@/components/home/Home/Home";
 import Root from "@/layout/Root";
-import Home from "@/components/home/Home";
 import { createBrowserRouter } from "react-router-dom";
+import About from "./../components/about/About";
+import Featured from "@/components/featured/Featured";
+import Products from "./../components/products/Products";
+import Contact from "./../contact/Contact";
 
 const router = createBrowserRouter([
   {
@@ -9,8 +13,24 @@ const router = createBrowserRouter([
 
     children: [
       {
-        path: "/home",
+        path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/featured",
+        element: <Featured></Featured>,
+      },
+      {
+        path: "/products",
+        element: <Products></Products>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
       },
     ],
   },
